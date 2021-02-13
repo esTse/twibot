f0bac91aa171694314ed457beeb27b2c5d0aeb35
esTse <40002393+esTse@users.noreply.github.com>
2021-02-13 12:16:32 +0100

    Add files via upload

---
+++ twibot.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+from tweepy import StreamListener, Stream
+import tweepy
+import json
+import requests
+from dotenv import load_dotenv
+import os
+
+class TwitterBot(StreamListener):
+    def __init__(self, keywords=[]):
+        load_dotenv()
+        # Auth env config
+        consumer_key = os.getenv("CONSUMER_KEY")
+        consumer_secret = os.getenv("CONSUMER_SECRET")
+        access_token = os.getenv("ACCESS_TOKEN")
+        access_secret = os.getenv("ACCESS_SECRET")
+        # Auth handler
+        self.auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        self.auth.set_access_token(access_token, access_secret)
+        # Telegram config
+        self.telegram_token = os.getenv("TELEGRAM_TOKEN")
+        self.telgram_room = os.getenv("TELEGRAM_ROOM")
+        # Twitter targets to scrape
+        self.targets = os.getenv("TARGETS")
+        self.keywords = keywords
+
+    def telegram_bot_sendtext(self, bot_token, bot_chatID, bot_message):
+        send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&text=' + bot_message
+        response = requests.get(send_text)
+
+    def on_data(self, data):
+        try:
+            # process stream data here
+            tweet = json.loads(data)
+            print(tweet)
+            try:
+               text = tweet['extended_tweet']['full_text']
+               url = tweet['entities']['urls'][0]['url']
+            except:
+               text = tweet['text']
+               url = "https://twitter.com/" + tweet['user']['screen_name'] + "/status/" + tweet['id_str']
+
+            for i in self.keywords:
+                if i in text.lower():
+                   self.telegram_bot_sendtext(self.telegram_token, self.telgram_room, url)
+
+        except KeyError:
+               print("Tweet deleted or API attribute not found.")
+
+    def on_error(self, status):
+         print(status)
+
+if __name__ == "__main__":
+    keywords = ["doge", "xrp"]
+    bot = TwitterBot(keywords)
+    twitterStream = Stream(bot.auth, bot, tweet_mode= 'extended')
+    twitterStream.filter(follow=bot.targets.split(";"))
